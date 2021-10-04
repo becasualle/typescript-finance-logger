e7dfdca3298a7b8deca99445ca31bfd1e9ab2ce3
Mark <mark.nelubin@gmail.com>
2021-10-04 11:07:09 +0300

    Objects&Arrays

--- sandbox.js
+++ sandbox.js
@@ -1,8 +1,32 @@
-var character = 'mark';
-var age = 30;
-var isBlackBelt = false;
-character = 'sveta';
-var circ = function (diameter) {
-    return diameter * Math.PI;
+// arrays
+var names = ['mark', 'sveta', 'alex'];
+// can't change array type itself
+// names = 'hello'
+names.push('roma'); //can add string
+//can not add numbers
+// names.push(3); 
+// names[0] = 3;
+var numbers = [10, 20, 30, 40];
+numbers.push(25);
+// numbers.push('mark');
+// numbers[1] = 'mark';
+var mixed = ['mark', 15, 'sveta', 30];
+// can add types that contains mixed array
+mixed.push('alex');
+mixed.push(189);
+mixed[3] = 317;
+// objects
+var ninja = {
+    name: 'mark',
+    status: 'cool',
+    age: 30
 };
-console.log(circ(7.5));
+// ninja always has to be object, name always has to be string
+// can't add new properties to the object
+// ninja.skills = ['coding', 'gaming'];
+// we can update object but only if it matches original structure (same properties)
+ninja = {
+    name: 'mark',
+    status: 'cool',
+    age: 30
+};
--- sandbox.ts
+++ sandbox.ts
@@ -1,12 +1,39 @@
-let character = 'mark';
+// arrays
+let names = ['mark', 'sveta', 'alex'];
 
-let age = 30;
-let isBlackBelt = false; 
+// can't change array type itself
+// names = 'hello'
 
-character = 'sveta';
+names.push('roma'); //can add string
+//can not add numbers
+// names.push(3); 
+// names[0] = 3;
 
-const circ = (diameter: number) => {
-  return diameter * Math.PI;
-}
+let numbers = [10, 20, 30, 40];
+numbers.push(25);
+// numbers.push('mark');
+// numbers[1] = 'mark';
 
-console.log(circ(7.5));
+let mixed = ['mark', 15, 'sveta', 30];
+// can add types that contains mixed array
+mixed.push('alex');
+mixed.push(189);
+mixed[3] = 317;
+
+// objects
+let ninja = {
+  name: 'mark',
+  status: 'cool',
+  age: 30
+};
+// ninja always has to be object, name always has to be string
+
+// can't add new properties to the object
+// ninja.skills = ['coding', 'gaming'];
+
+// we can update object but only if it matches original structure (same properties)
+ninja = {
+  name: 'mark',
+  status: 'cool',
+  age: 30
+};

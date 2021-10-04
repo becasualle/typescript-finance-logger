c489cbea85abfce31cd1a3b370095c9e97a8c5f5
Mark <mark.nelubin@gmail.com>
2021-10-04 12:14:36 +0300

    dynamic types

--- sandbox.js
+++ sandbox.js
@@ -1,39 +1,12 @@
-// EXPLICIT TYPES
-var character;
-var age;
-var isLoggedIn;
-// age = 'mark'; can't do this
-age = 30;
-isLoggedIn = true;
-// ARRAYS
-var ninjas; //array of strings
-// doesn't allowed
-// ninjas = [10, 23]
-ninjas = ['mark', 'sveta'];
-// array is undefined so we can't push values
-// ninjas.push('mark')
-var students = [];
-students.push('mark', 'sveta');
-// UNION TYPES
+var age = 25;
+age = 'peter';
+age = true;
+age = { name: 'mark' };
 var mixed = [];
-mixed.push('hello');
-mixed.push(20);
-mixed.push(false);
-// ['hello', 20, false]
-var uid;
-uid = '123';
-uid = 123;
-// OBJECTS
-var ninjaOne;
-ninjaOne = {
+mixed.push(5);
+mixed.push(true);
+var ninja;
+ninja = {
     name: 'mark',
-    age: 30
-};
-ninjaOne = []; //array is type of object
-// create object that have to contain 3 properties with selected types
-var ninjaTwo;
-ninjaTwo = {
-    name: 'mark',
-    age: 15,
-    beltColor: 'black'
+    age: '30'
 };
--- sandbox.ts
+++ sandbox.ts
@@ -1,54 +1,18 @@
-// EXPLICIT TYPES
-let character: string;
-let age: number;
-let isLoggedIn: boolean;
+let age: any = 25;
+age = 'peter';
+age = true;
+age = {name: 'mark'};
 
-// age = 'mark'; can't do this
-age = 30;
-isLoggedIn = true;
+let mixed: any[] = [];
+mixed.push(5);
+mixed.push(true);
 
-// ARRAYS
-let ninjas: string[]; //array of strings
-
-// doesn't allowed
-// ninjas = [10, 23]
-
-ninjas = ['mark', 'sveta'];
-
-// array is undefined so we can't push values
-// ninjas.push('mark')
-
-let students: string[] = [];
-students.push('mark', 'sveta');
-
-// UNION TYPES
-let mixed: (string|number|boolean)[] = [];
-mixed.push('hello');
-mixed.push(20);
-mixed.push(false);
-// ['hello', 20, false]
-
-let uid: string|number;
-uid = '123';
-uid = 123;
-
-// OBJECTS
-let ninjaOne: object;
-ninjaOne = {
-  name: 'mark',
-  age: 30
-}
-ninjaOne=[]; //array is type of object
-
-// create object that have to contain 3 properties with selected types
-let ninjaTwo: {
-  name: string,
-  age: number,
-  beltColor: string
+let ninja : {
+  name: any,
+  age: any
 }
 
-ninjaTwo = {
+ninja = {
   name: 'mark',
-  age: 15,
-  beltColor: 'black',
+  age: '30'
 }

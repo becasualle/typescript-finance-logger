a9a2fb641c3c3f009f125b83837ae7336a297d39
Mark <mark.nelubin@gmail.com>
2021-10-05 10:00:37 +0300

    type aliases

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,25 +1,13 @@
 "use strict";
-var greet;
-greet = function () {
-    console.log('hello world');
+// const logDetails = (uid: string|number, item:string) => {
+//     console.log(`${item} has a uid of ${uid}`)
+// }
+var logDetails = function (uid, item) {
+    console.log(item + " has a uid of " + uid);
 };
-// c - optional parameter c?: number|string
-// c - optional parameter with default value of 10 c: number|string = 10
-// add optional parameters at the end
-// when we don't return - it showes that returns void
-// (a: number, b: number, c: number|string = 10): void
-var add = function (a, b, c) {
-    if (c === void 0) { c = 10; }
-    console.log(a + b);
-    console.log(c);
+// const greet = (user: {name:string, uid:string|number}) => {
+//     console.log(`${user.name} says hello`)
+// }
+var greet = function (user) {
+    console.log(user.name + " says hello");
 };
-add(5, 10, '20');
-// can rewrite to show that this function returns value
-// const minus = (a: number, b: number): number
-var minus = function (a, b) {
-    return a - b;
-};
-// result will inherit type of it's arguments
-var result = minus(10, 7);
-// can't use another value
-// result = 'something else'
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,27 +1,18 @@
-let greet: Function;
+type StringOrNum = string|number;
+type objWithName = {name: string, uid: StringOrNum}
 
-greet = () => {
-    console.log('hello world');
+// const logDetails = (uid: string|number, item:string) => {
+//     console.log(`${item} has a uid of ${uid}`)
+// }
+
+const logDetails = (uid: StringOrNum, item:string) => {
+    console.log(`${item} has a uid of ${uid}`)
 }
 
-// c - optional parameter c?: number|string
-// c - optional parameter with default value of 10 c: number|string = 10
-// add optional parameters at the end
-// when we don't return - it showes that returns void
-// (a: number, b: number, c: number|string = 10): void
-const add = (a: number, b: number, c: number|string = 10) =>{
-    console.log(a+b);
-    console.log(c);
-}
+// const greet = (user: {name:string, uid:string|number}) => {
+//     console.log(`${user.name} says hello`)
+// }
 
-add(5, 10, '20');
-
-// can rewrite to show that this function returns value
-// const minus = (a: number, b: number): number
-const minus = (a: number, b: number) => {
-    return a-b;
+const greet = (user: objWithName) => {
+    console.log(`${user.name} says hello`)
 }
-// result will inherit type of it's arguments
-let result = minus(10,7);
-// can't use another value
-// result = 'something else'
